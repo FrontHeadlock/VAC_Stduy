a79849e3d981eeffca0c03639da5f81662895566
Kyumin Park <southvi1@naver.com>
2024-07-28 18:06:47 +0900

    first commit

---
+++ src/Comment.java
@@ -0,0 +1,10 @@
+public class Comment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello java 1");     // hello java1을 출력한다
+        System.out.println("Hello java 2");
+
+//        System.out.println("Hello java 3");
+//        System.out.println("Hello java 4");
+    }
+}
