f2bfd23c4b91658bce1e533339d26ae3813a915f
Kyumin Park <southvi1@naver.com>
2024-08-04 16:09:23 +0900

    first commit

---
+++ src/variable/Var1Ex1.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Var1Ex1 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 4;
+        b = 3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+
+        int num1,num2;
+        num1 = 10;
+        num2 = 20;
+        int sum = num1+num2;
+        System.out.println(sum);
+
+        long longVar = 100000000L;
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanVar = true;
+        System.out.println("booleanVar = " + booleanVar);
+
+    }
+}
---
+++ src/variable/Var4.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b,c,d;
+
+        a=1;
+        b=2;
+
+    }
+}
