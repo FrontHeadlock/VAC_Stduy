caa518480b0b9344e19b2d186eeae606eaec6118
Kyumin Park <southvi1@naver.com>
2024-08-07 19:11:57 +0900

    0807

---
+++ src/EX/EvenOddEx.java
@@ -0,0 +1,9 @@
+package EX;
+
+public class EvenOddEx {
+    public static void main(String[] args) {
+        int x = 21;
+        String result = (x%2==0)? "짝수" : "홀수";
+        System.out.printf("x = %d , %s",x,result);
+    }
+}
