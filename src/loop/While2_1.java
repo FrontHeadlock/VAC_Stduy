9ee261f86e4ca5f9cf34632896d510d370297eb9
Kyumin Park <southvi1@naver.com>
2024-08-10 11:30:09 +0900

    0810

---
+++ src/loop/While2_1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While2_1 {
+    public static void main(String[] args) {
+        int sum =0;
+        sum += 1;
+        System.out.printf("i = %d sum = %d\n",1,sum);
+        sum += 2;
+        System.out.printf("i = %d sum = %d\n",2,sum);
+        sum += 3;
+        System.out.printf("i = %d sum = %d\n",3,sum);
+    }
+}
