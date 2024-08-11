17bab8b527c8e2f0d903cdfd95173ac7cbd2eb9f
Kyumin Park <southvi1@naver.com>
2024-08-11 12:44:45 +0900

    0811

---
+++ src/loop/ForEx2.java
@@ -0,0 +1,9 @@
+package loop;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+        for(int num =2, cnt = 1; cnt <= 10; num+=2,cnt++){
+            System.out.println(num);
+        }
+    }
+}
