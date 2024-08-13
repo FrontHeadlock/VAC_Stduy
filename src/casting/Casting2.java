5f31ee7979f9f271b92e63b92db86ff75bd19bd6
Kyumin Park <southvi1@naver.com>
2024-08-13 17:42:45 +0900

    0813

--- src/casting/Casting2.java
+++ src/casting/Casting2.java
@@ -3,6 +3,9 @@ package casting;
 public class Casting2 {
     public static void main(String[] args) {
         double doubleValue = 1.5;
-        int intValue =
+        int intValue = 0;
+
+        intValue = (int)doubleValue;
+        System.out.println(intValue);
     }
 }
