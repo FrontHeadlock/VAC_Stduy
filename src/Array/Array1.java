376ec47f7ef3c7091e23854bad6ee3cad922089e
Kyumin Park <southvi1@naver.com>
2024-08-20 22:57:01 +0900

    0820

---
+++ src/Array/Array1.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] students;
+        students = new int[5];
+        students[0] = 90;
+        students[1] = 80;
+        students[2] = 70;
+        students[3] = 60;
+        students[4] = 50;
+
+        System.out.printf("학생1 점수 : %d\n",students[0]);
+        System.out.printf("학생2 점수 : %d\n",students[1]);
+        System.out.printf("학생3 점수 : %d\n",students[2]);
+        System.out.printf("학생4 점수 : %d\n",students[3]);
+        System.out.printf("학생5 점수 : %d\n",students[4]);
+    }
+}
