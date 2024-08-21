7e05c0b8179e2fa21d75ad93b74c308404a8fba7
Kyumin Park <southvi1@naver.com>
2024-08-21 22:56:49 +0900

    0821

---
+++ src/Array/ex/ArrayEx2.java
@@ -0,0 +1,11 @@
+package Array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
