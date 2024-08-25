c754bc1f54da5c95eaf1e4e8e2bde6cd0c89cab8
Kyumin Park <southvi1@naver.com>
2024-08-25 15:04:08 +0900

    0825

---
+++ src/method/Method1.java
@@ -0,0 +1,14 @@
+package method;
+
+public class Method1 {
+    public static void main(String[] args) {
+        int a =1;
+        int b =2;
+        System.out.printf("%d + %d = %d (연산수행)\n",a,b,a+b);
+
+        int x =10;
+        int y =20;
+        System.out.printf("%d + %d = %d (연산수행)",x,y,x+y );
+
+    }
+}
