9ae5b440390af1ba18cf404007e154e37209eac7
Kyumin Park <southvi1@naver.com>
2024-08-27 20:58:34 +0900

    0827

---
+++ src/method/MethodCasting1.java
@@ -0,0 +1,11 @@
+package method;
+
+public class MethodCasting1 {
+    public static void main(String[] args) {
+        int num=15;
+        printNumber(num);
+    }
+    public static void printNumber(int n){
+        System.out.println("숫자 : "+n);
+    }
+}
